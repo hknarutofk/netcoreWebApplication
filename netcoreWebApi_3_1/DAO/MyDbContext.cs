55471ba8e40731d6afb2bc0422a6ecbc7b222782
hknarutofk <289632785@qq.com>
2020-11-10 08:37:53 +0800

    .net core 3.1 mysql api 示例程序

---
+++ netcoreWebApi_3_1/DAO/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using netcoreWebApi_3_1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreWebApi_3_1.DAO
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Sbtest1> Sbtest1 { get; set; }
+    }
+}
---
+++ netcoreWebApi_3_1/Entities/Sbtest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreWebApi_3_1.Entities
+{
+    [Table("sbtest1")]
+    public class Sbtest1
+    {
+        [Column("id")]
+        public int id { get; set; }
+        [Column("k")]
+        public int k { get; set; }
+        [Column("c")]
+        public string c { get; set; }
+        [Column("pad")]
+        public string pad { get; set; }
+    }
+}
